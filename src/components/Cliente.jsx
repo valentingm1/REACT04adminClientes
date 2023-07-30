616528cbd913e56fa57b96575b092b6fd9b77bfe
NeedAssistance <gomarchjjba@gmail.com>
2023-07-30 04:57:26 -0300

    Tabla mostrando toda la informacion de clientes + botones Editar/Eliminar

--- src/components/Cliente.jsx
+++ src/components/Cliente.jsx
@@ -2,11 +2,28 @@
 
 function Cliente({cliente}) {
 
-    const {nombre, empresa, mail, telefono, id} = cliente
+    const {nombre, empresa, email, telefono, id} = cliente
 
   return (
-    <tr>
-        <td className="p-6">{nombre}</td>
+    <tr className="border-b">
+        <td className="p-6 space-y-2">
+          <p className="text-2xl text-gray-800">{nombre}</p>
+          <p>{empresa}</p>
+        </td>
+
+        <td className="p-6 mx-4">
+        <p className="text-gray-600 "><span className="text-gray-800 font-bold">EMAIL: </span>{email}</p>
+        <p className="text-gray-600"><span className="text-gray-800 font-bold">TEL: </span>{telefono}</p>
+        </td>
+
+        <td className="p-6 flex justify-center gap-4 ">
+          <button className="text-blue-600 hover:to-blue-700 uppercase font-bold text-xs transition" type="button">
+            Editar
+          </button>
+          <button className="text-red-400 hover:to-red-600 uppercase font-bold text-xs transition" type="button">
+            Eliminar
+          </button>
+        </td>
     </tr>
   )
 }
