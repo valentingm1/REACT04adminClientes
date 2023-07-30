73ae30320503859122d996d0e9025d6832f53444
NeedAssistance <gomarchjjba@gmail.com>
2023-07-29 23:12:15 -0300

    Creando pages, componente Layout, agregando Link y Outlet

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './components/Layout'
+import NuevoCliente from './pages/NuevoCliente'
+import Index from './pages/Index'
 
 const router = createBrowserRouter([
   {
   path: "/",
-  element: ""
-  },
-  {
-    path: "/",
-    element:""
+  element: <Layout/>,
+  children: [
+    {
+      index: true,
+      element: <Index />
+    },
+    {
+    path: "/clientes/nuevo",
+    element: <NuevoCliente/>,
+    }
+  ]
   }
 ])
 
---
+++ src/pages/NuevoCliente.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function NuevoCliente() {
+  return (
+    <div>NuevoCliente</div>
+  )
+}
+
+export default NuevoCliente
